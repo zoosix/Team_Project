e78d74ccc4b7270fa5283a7cfb1dfe5c79e8da07
joointhezoo <488654@naver.com>
2015-10-29 17:26:17 +0900

    05:26

--- Team_MainProject/src/com/model/MyWishModel.java
+++ Team_MainProject/src/com/model/MyWishModel.java
@@ -8,7 +8,7 @@ public class MyWishModel implements Model{
 	@Override
 	public String handlerRequest(HttpServletRequest req, HttpServletResponse res) throws Exception {
 		// TODO Auto-generated method stub
-		req.setAttribute("jsp", "../mypage/model.jsp");
+		req.setAttribute("jsp", "../mypage/wish.jsp");
 		return "main/main.jsp";
 	}
 }
