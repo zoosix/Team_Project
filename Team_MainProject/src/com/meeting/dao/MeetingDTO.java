4faa3878a024722664580d3d407557fdc3f3f887
minsu na <wns3179@naver.com>
2015-11-06 16:18:24 +0900

    master edit create page

---
+++ Team_MainProject/src/com/meeting/dao/MeetingDTO.java
@@ -0,0 +1,125 @@
+package com.meeting.dao;
+
+public class MeetingDTO {
+	private int m_no;
+	private String m_lang1;
+	private String m_lang2;
+	private int m_lang1num;
+	private int m_lang2num;
+	private String m_title;
+	private String m_summary;
+	private String m_content;
+	private String m_meetingDate;
+	private String m_partDate;
+	private String m_place;
+	private String m_location;
+	private String m_email;
+	private String m_tel;
+	private String m_end;
+	private String m_filename;
+	private int m_filesize;
+	
+	public int getM_filesize() {
+		return m_filesize;
+	}
+	public void setM_filesize(int m_filesize) {
+		this.m_filesize = m_filesize;
+	}
+	public int getM_no() {
+		return m_no;
+	}
+	public void setM_no(int m_no) {
+		this.m_no = m_no;
+	}
+	public String getM_lang1() {
+		return m_lang1;
+	}
+	public void setM_lang1(String m_lang1) {
+		this.m_lang1 = m_lang1;
+	}
+	public String getM_lang2() {
+		return m_lang2;
+	}
+	public void setM_lang2(String m_lang2) {
+		this.m_lang2 = m_lang2;
+	}
+	public int getM_lang1num() {
+		return m_lang1num;
+	}
+	public void setM_lang1num(int m_lang1num) {
+		this.m_lang1num = m_lang1num;
+	}
+	public int getM_lang2num() {
+		return m_lang2num;
+	}
+	public void setM_lang2num(int m_lang2num) {
+		this.m_lang2num = m_lang2num;
+	}
+	public String getM_title() {
+		return m_title;
+	}
+	public void setM_title(String m_title) {
+		this.m_title = m_title;
+	}
+	public String getM_summary() {
+		return m_summary;
+	}
+	public void setM_summary(String m_summary) {
+		this.m_summary = m_summary;
+	}
+	public String getM_content() {
+		return m_content;
+	}
+	public void setM_content(String m_content) {
+		this.m_content = m_content;
+	}
+	public String getM_meetingDate() {
+		return m_meetingDate;
+	}
+	public void setM_meetingDate(String m_meetingDate) {
+		this.m_meetingDate = m_meetingDate;
+	}
+	public String getM_partDate() {
+		return m_partDate;
+	}
+	public void setM_partDate(String m_partDate) {
+		this.m_partDate = m_partDate;
+	}
+	public String getM_place() {
+		return m_place;
+	}
+	public void setM_place(String m_place) {
+		this.m_place = m_place;
+	}
+	public String getM_location() {
+		return m_location;
+	}
+	public void setM_location(String m_location) {
+		this.m_location = m_location;
+	}
+	public String getM_email() {
+		return m_email;
+	}
+	public void setM_email(String m_email) {
+		this.m_email = m_email;
+	}
+	public String getM_tel() {
+		return m_tel;
+	}
+	public void setM_tel(String m_tel) {
+		this.m_tel = m_tel;
+	}
+	public String getM_end() {
+		return m_end;
+	}
+	public void setM_end(String m_end) {
+		this.m_end = m_end;
+	}
+	public String getM_filename() {
+		return m_filename;
+	}
+	public void setM_filename(String m_filename) {
+		this.m_filename = m_filename;
+	}
+	
+}
