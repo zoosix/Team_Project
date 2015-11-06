4fdca59c702a12c5a49e2d7178ae73947db8defb
minsu na <wns3179@naver.com>
2015-11-06 16:47:27 +0900

    업로드 경로 수정

--- Team_MainProject/WebContent/meeting/se2/sample/photo_uploader/attach_photo.js
+++ Team_MainProject/WebContent/meeting/se2/sample/photo_uploader/attach_photo.js
@@ -334,7 +334,7 @@
     	var tempFile,
     		sUploadURL;
     	
-    	sUploadURL= '/NadeulmokProject/meeting/se2/sample/photo_uploader/file_uploader_html5.jsp'; 	//upload URL
+    	sUploadURL= '/Team_MainProject/meeting/se2/sample/photo_uploader/file_uploader_html5.jsp'; 	//upload URL
     	
     	//파일을 하나씩 보내고, 결과를 받음.
     	for(var j=0, k=0; j < nImageInfoCnt; j++) {
@@ -479,8 +479,8 @@
  	function callFileUploader (){
  		oFileUploader = new jindo.FileUploader(jindo.$("uploadInputBox"),{
  			//sUrl  : location.href.replace(/\/[^\/]*$/, '') + '/file_uploader.php',	//샘플 URL입니다.
- 			sUrl  : '/NadeulmokProject/meeting/se2/sample/photo_uploader/file_uploader.jsp',	//샘플 URL입니다.
- 	        sCallback : '/NadeulmokProject/meeting/se2/sample/photo_uploader/callback.html',	//업로드 이후에 iframe이 redirect될 콜백페이지의 주소
+ 			sUrl  : '/Team_MainProject/meeting/se2/sample/photo_uploader/file_uploader.jsp',	//샘플 URL입니다.
+ 	        sCallback : '/Team_MainProject/meeting/se2/sample/photo_uploader/callback.html',	//업로드 이후에 iframe이 redirect될 콜백페이지의 주소
  	    	sFiletype : "*.jpg;*.png;*.bmp;*.gif",						//허용할 파일의 형식. ex) "*", "*.*", "*.jpg", 구분자(;)	
  	    	sMsgNotAllowedExt : 'JPG, GIF, PNG, BMP 확장자만 가능합니다',	//허용할 파일의 형식이 아닌경우에 띄워주는 경고창의 문구
  	    	bAutoUpload : false,									 	//파일이 선택됨과 동시에 자동으로 업로드를 수행할지 여부 (upload 메소드 수행)
